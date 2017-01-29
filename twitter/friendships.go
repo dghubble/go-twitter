3d5a6ac72d184e80464fee1734507e72dfe2858e
yyoshiki41 <yyoshiki41@gmail.com>
2017-01-29 20:56:28 +0900

    Add friendships/show

--- twitter/friendships.go
+++ twitter/friendships.go
@@ -53,3 +53,57 @@ func (s *FriendshipService) Destroy(params *FriendshipDestroyParams) (*User, *ht
 	resp, err := s.sling.New().Post("destroy.json").QueryStruct(params).Receive(user, apiError)
 	return user, resp, relevantError(err, *apiError)
 }
+
+// FriendshipShowParams are paramenters for FriendshipService.Show
+type FriendshipShowParams struct {
+	SourceID         int64  `url:"source_id,omitempty"`
+	SourceScreenName string `url:"source_screen_name,omitempty"`
+	TargetID         int64  `url:"target_id,omitempty"`
+	TargetScreenName string `url:"target_screen_name,omitempty"`
+}
+
+// Show returns the relationship between two arbitrary users.
+// Requires a user auth or an app context.
+// https://dev.twitter.com/rest/reference/get/friendships/show
+func (s *FriendshipService) Show(params *FriendshipShowParams) (*Relationship, *http.Response, error) {
+	relationship := new(Relationship)
+	apiError := new(APIError)
+	resp, err := s.sling.New().Get("show.json").QueryStruct(params).Receive(relationship, apiError)
+	return relationship, resp, relevantError(err, *apiError)
+}
+
+// Relationship represents the relationship between two arbitrary users.
+type Relationship struct {
+	*RelationshipResponse `json:"relationship"`
+}
+
+// RelationshipResponse is struct for relationship API response.
+type RelationshipResponse struct {
+	Source *RelationshipSource `json:"source"`
+	Target *RelationshipTarget `json:"target"`
+}
+
+// RelationshipSource represents the source user.
+type RelationshipSource struct {
+	ID                   int64  `json:"id"`
+	IDStr                string `json:"id_str"`
+	ScreenName           string `json:"screen_name"`
+	Following            bool   `json:"following"`
+	FollowedBy           bool   `json:"followed_by"`
+	CanDm                bool   `json:"can_dm"`
+	Blocking             *bool  `json:"blocking"`
+	Muting               *bool  `json:"muting"`
+	AllReplies           *bool  `json:"all_replies"`
+	WantRetweets         *bool  `json:"want_retweets"`
+	MarkedSpam           *bool  `json:"marked_spam"`
+	NotificationsEnabled *bool  `json:"notifications_enabled"`
+}
+
+// RelationshipTarget represents the target user.
+type RelationshipTarget struct {
+	IDStr       string `json:"id_str"`
+	ID          int64  `json:"id"`
+	ScreenName  string `json:"screen_name"`
+	Following   bool   `json:"following"`
+	FollowingBy bool   `json:"followed_by"`
+}
