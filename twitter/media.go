b0ffe9cbd93e09f7be65efe6e5ff9eafe0b3663d
Jason Lentink <jason@mediamonks.com>
2017-11-13 14:43:09 -0600

    gofmt/golint/comments added

--- twitter/media.go
+++ twitter/media.go
@@ -9,24 +9,28 @@ import (
 
 const twitterUploadAPI = "https://upload.twitter.com/1.1/"
 
+// Media is the structure received from Twitter after upload
 type Media struct {
-	MediaId             int64       `json:"media_id"`
-	MediaIdString       string      `json:"media_id_string"`
+	MediaID             int64       `json:"media_id"`
+	MediaIDString       string      `json:"media_id_string"`
 	Size                int32       `json:"size"`
 	ExpiresAfterSeconds int32       `json:"expires_after_secs"`
 	Image               *MediaImage `json:"image"`
 }
 
+//MediaImage is the structure within Media that contains image information
 type MediaImage struct {
 	ImageType string `json:"image_type"`
 	Width     int32  `json:"w"`
 	Height    int32  `json:"h"`
 }
 
+// MediaParams is the struct expected by Twitter for uploading
 type MediaParams struct {
 	MediaData string `url:"media_data"`
 }
 
+// MediaService provides methods for accessing Twitter upload API endpoints.
 type MediaService struct {
 	sling *sling.Sling
 }
@@ -37,6 +41,7 @@ func newMediaService(sling *sling.Sling) *MediaService {
 	}
 }
 
+// Encode transfers the data of the image into base64
 func (m *MediaService) Encode(path string) (string, error) {
 	buff, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -46,6 +51,8 @@ func (m *MediaService) Encode(path string) (string, error) {
 	return base64.StdEncoding.EncodeToString(buff), nil
 }
 
+// UploadFile is a uploader for an image to twitter
+// https://developer.twitter.com/en/docs/media/upload-media/overview
 func (m *MediaService) UploadFile(filePath string) (*Media, *http.Response, error) {
 	EncodedImage, _ := m.Encode(filePath)
 	apiError := new(APIError)
