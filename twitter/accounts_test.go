798d858f7e4826e7a4ec7de3ddc45071ad935641
Julian Yap <julianokyap@gmail.com>
2022-08-22 01:12:53 -0700

    Add AccountService UpdateProfileImage test case

--- twitter/accounts_test.go
+++ twitter/accounts_test.go
@@ -43,3 +43,24 @@ func TestAccountService_UpdateProfile(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expected, user)
 }
+
+func TestAccountService_UpdateProfileImage(t *testing.T) {
+	base64PNG := "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAABCFBMVEX////+/v7++/z9+Pr99/r89vn89Pj78vf66/P66vL56fH56PD45u/45e/45O734ez23ur23On12uj12ef11+b11uX00+Pz0eLz0OHyzN/wxNrww9nwwtnvwtjuvdbuvNXtutTtudPtuNLtt9LstdHstNDss9Drss/rr83qrMvpqMjoo8XmmsDlmb/lmL7klLzjkbrjj7nii7fiirbhirXghbPghbLghLLfgLDff6/efa7dd6rddqncdqnccqfbcabbb6Xaa6PZaKHYZJ7YYp3XX5vVV5bUVZXTUJHSTZDRSI3QQ4rPP4fPPofOOoTNOYPNN4LMNYHLMX7LMH7KLXzKLHvKK3vKKnp2VKJ7AAAA9ElEQVR42u3JRWIEMQxE0QozMzM2hJmZaajuf5PYHfAcQD2regtb0oeIiIiIiIiIiOSpbTjTh47s7wfQOzffXxUMHDBzjpPsv0Hzpf/jEAwsxAd8i+MxPHE3TdNxnPJsYuZ4KgQTS7xwb2O5UO+3uq9yS1Wwss3EvSN8WVhYGAWeeTcUgpVbTrp3nd4WsFwkDxtCsPHBVvce8SJJkkEA7TtF7oVgopOv/nvg6P9pms8hmJjmtXvri5UmeCt1wCLvf4OVDW66d4ClB6e7i4/7Z59c+w1Wrjjr3lV6xfqeuwr5HoWQj6bejjrUhoiIiIiIiIiIfANKKjEabNmIvgAAAABJRU5ErkJggg=="
+	httpClient, mux, server := testServer()
+	defer server.Close()
+
+	mux.HandleFunc("/1.1/account/update_profile_image.json", func(w http.ResponseWriter, r *http.Request) {
+		assertMethod(t, "POST", r)
+		assertQuery(t, map[string]string{"image": base64PNG}, r)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"image": "%s"}`, base64PNG)
+	})
+
+	client := NewClient(httpClient)
+	params := &AccountUpdateProfileImageParams{Image: base64PNG}
+	user, _, err := client.Accounts.UpdateProfileImage(params)
+	expected := &User{DefaultProfileImage: false}
+	assert.Nil(t, err)
+	assert.Equal(t, expected, user)
+
+}
